069479eb50e94932e8841dc26ee57ea0aa53b7b8
Ravi Varma T <29795226+ravivarmathotakura@users.noreply.github.com>
2020-07-18 20:18:00 +0530

    Update app.py
    Release Note:
    Version(Beta): 0.2
    Added new feature like Home, Pandas Profiling and Sweetviz

--- app.py
+++ app.py
@@ -6,20 +6,34 @@ Created on Fri Jul 10 20:04:45 2020
 @author: ravivarma
 """
 
-# Core Pkgs
+## Core
 import streamlit as st
+import streamlit.components.v1 as components
+from streamlit_pandas_profiling import st_profile_report
 
-# EDA Pkgs
+## Custome Components
+import sweetviz as sv
+
+def st_display_sweetviz(report_html, width = 1000, height = 500):
+    report_file = codecs.open(report_html, 'r')
+    page = report_file.read()
+    components.html(page,width=width,height=height,scrolling=True)
+
+## EDA
 import pandas as pd
 import numpy as np
+import codecs
+from pandas_profiling import ProfileReport
 
-# Data Viz Pkgs
+## Data Visualization
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
+# COLOR = "black"
+# BACKGROUND_COLOR = "#fff"
 import seaborn as sns
 
-# Ml Pkgs
+## Machine Learning
 from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -33,11 +47,43 @@ def main():
     """AutoML Web App Tool with Streamlit"""
     
     st.title("AutoML WebApp")
-    st.text("Version(Beta): 0.1")
+    st.text("Version(Beta): 0.2")
     
-    activities = ["EDA", "Plot", "Model Building", "About"]
+    menu = ["Home", "Pandas Profile", "Sweetviz", "EDA", "Plot", "Model Building", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+        
+    if choice == 'Home':
+        st.markdown('**Data Analysis, Visualization** and Machine Learning **Model Building** in an interactive **WebApp** for Data Scientist/Data Engineer/Business Analyst.  \n\nThe purpose of this app is to create a **quick Business Insights**.  \n\nAutoML WebApp built with **Streamlit framework** using **Pandas** and **Numpy** for Data Analysis, **Matplotlib** and **Seaborn** for Data Visualization, **SciKit-Learn** for Machine Learning Model.')
+#         st.markdown('**Demo URL**: https://automlwebapp.herokuapp.com/')
+        st.header("Silent Features")
+        st.markdown('* User can browse or upload file(Dataset) in .csv or .txt format.  \n* User can get the details of dataset like No. of rows & Columns, Can View Column list, Select Columns with rows to show, Dataset Summary like count, mean, std, min and max values.  \n* Several Data Visualizations like Correlation with HeatMap, PieChart and Plots like Area, Bar, Line, Box, KDE.  \n* User can built Models like LogisticRegression, LinearDiscriminantAnalysis, KNeighborsClassifier, DecisionTreeClassifier, GaussianNB, SVC.  \n* Model Evaluation with Accuracy, Mean and Standard Deviation.')
+
     
-    choice = st.sidebar.selectbox("Select Activity", activities)
+           
+    if choice == 'Pandas Profile':
+        st.subheader("Automated EDA with Pandas Profile")
+        
+        data = st.file_uploader("Upload Dataset", type=["csv", "txt"]) 
+        if data is not None:
+            df = pd.read_csv(data)
+            st.dataframe(df.head())
+            profile = ProfileReport(df)
+            st_profile_report(profile)
+    
+    if choice == 'Sweetviz':
+        st.subheader("Automated EDA with Sweetviz")
+        
+        data = st.file_uploader("Upload Dataset", type=["csv", "txt"]) 
+        if data is not None:
+            df = pd.read_csv(data)
+            st.dataframe(df.head())
+            if st.button("Generate Sweetviz Report"):
+            
+            ## WorkFlow
+                report = sv.analyze(df)
+                report.show_html()
+                st_display_sweetviz("SWEETVIZ_REPORT.html")
+    
     
     if choice == 'EDA':
         st.subheader("Exploratory Data Analysis")
@@ -104,7 +150,7 @@ def main():
         if st.button("Generate Plot"):
             st.success("Generating Customizable Plot of {} for {}".format(type_of_plot,selected_columns_names))
             
-            # Plot By Streamlit
+            ## Plot By Streamlit
             if type_of_plot == 'area':
                 cust_data = df[selected_columns_names]
                 st.area_chart(cust_data)
@@ -117,7 +163,7 @@ def main():
                 cust_data = df[selected_columns_names]
                 st.line_chart(cust_data)
             
-            # Custom Plot
+            ## Custom Plot
             elif type_of_plot:
                 cust_plot=df[selected_columns_names].plot(kind=type_of_plot)
                 st.write(cust_plot)
@@ -134,12 +180,12 @@ def main():
             st.dataframe(df.head())
             
             
-            # Model Building
+            ## Model Building
             X = df.iloc[:,0:-1]
             Y = df.iloc[:,-1]
             seed = 7
             
-            # Model
+            ## Model
             models = [] 
             models.append(("LR", LogisticRegression()))
             models.append(("LDA", LinearDiscriminantAnalysis()))
@@ -147,9 +193,9 @@ def main():
             models.append(("CART", DecisionTreeClassifier()))
             models.append(("NB", GaussianNB()))
             models.append(("SVM", SVC()))
-            # Evaluate each model in turn
+            ## Evaluate each model in turn
 
-            # List
+            ## List
             model_names = []
             model_mean = []
             model_std = []
@@ -173,19 +219,13 @@ def main():
                 st.json(all_models)
 
     elif choice == "About":
-        st.header("About")
-        st.markdown('**Data Analysis, Visualization** and Machine Learning **Model Building** in an interactive **WebApp** for Data Scientist/Data Engineer/Business Analyst.  \n\nThe purpose of this app is to create a **quick Business Insights**.  \n\nAutoML WebApp built with **Streamlit framework** using **Pandas** and **Numpy** for Data Analysis, **Matplotlib** and **Seaborn** for Data Visualization, **SciKit-Learn** for Machine Learning Model.')
-        #st.markdown('**Demo URL**: https://automlwebapp.herokuapp.com/')
-        st.header("Silent Features")
-        st.markdown('* User can browse file(Dataset) in .csv or .txt format.  \n* User can get the details of dataset like No. of rows & Columns, Can View Column list, Select Columns with rows to show, Dataset Summary like count, mean, std, min and max values.  \n* Several Data Visualizations like Correlation with HeatMap, PieChart and Plots like Area, Bar, Line, Box, KDE.  \n* User can built Models like LogisticRegression, LinearDiscriminantAnalysis, KNeighborsClassifier, DecisionTreeClassifier, GaussianNB, SVC.  \n* Model Evaluation with Accuracy, Mean and Standard Deviation.')
-        st.header("Author")
+        st.header("About Author")
         st.markdown("Hi, there! I'm **Ravi Varma**. I'm passionate about using data to extract decision making insight and help machines learn to make the world a better place. If you liked what you saw, want to have a chat with me about the **Data Science** or **Machine Learning Projects**, **Work Opportunities**, or collaboration, shoot an **email** at **ravivarmathotakura@gmail.com**")
         st.markdown('**Portfolio**: https://ravivarmathotakura.github.io/portfolio/')
-        #st.markdown('**GitHub**: https://github.com/ravivarmathotakura')
-        #st.markdown('**LinkedIn**: https://www.linkedin.com/in/ravivarmathotakura/')
         st.markdown('**Follow Me**: [@LinkedIn](https://www.linkedin.com/in/ravivarmathotakura/ "LinkedIn"), [@GitHub](https://github.com/ravivarmathotakura "GitHub")')
         st.subheader("Note")
         st.text("The author is not responsible for any misuse the program. \nAny contribution or suggestions are most welcome.")
+        
     
 if __name__ == '__main__':
     main()
